d5c0403127860d3695136351c6b72ee89b11f636
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-12-05 08:51:57 +0530

    Update pravavi.js

--- pravavi.js
+++ pravavi.js
@@ -11,6 +11,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
  PHONE: process.env.NUMBER === undefined ? '+94779529221' : process.env.NUMBER,
- XF_ZERO: process.env.XF_ZERO === undefined ? '009' : process.env.XF_ZERO,   
+ XF_ZERO: process.env.XF_ZERO === undefined ? '009' : process.env.XF_ZERO,
+ SAFE_MODE: process.env.SAFE_MODE === undefined ? 'false' : process.env.SAFE_MODE,   
  OA_NAME: process.env.DEPLOYER === undefined ? 'Kaweesha chamod' : process.env.DEPLOYER,    
 };
