720e1a045ade98b4a483d07ef2d57d9bda6ded82
KAWEESHA CHAMOD <88616416+Mr-WHITE-SHADOW@users.noreply.github.com>
2021-12-31 09:54:38 +0530

    Create system_status.js

---
+++ plugins/system_status.js
@@ -0,0 +1,59 @@
+const Julie = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+const chalk = require('chalk');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('system_stats');
+
+const SYSDTXT = 'hello'
+const VER = 'hi'
+const MSG = 'Mr white shadow'
+
+
+Julie.addCommand({pattern: 'alive', fromMe: true, desc: 'Lang.ALIVE_DESC'}, (async (message, match) => {
+
+    var a_plk =  "https://telegra.ph/file/9027f964886da41d44400.jpg" ;
+    var BUTTHANDLE = '.';
+ 
+
+    var logo = await axios.get (a_plk, { responseType: 'arraybuffer' })
+    var PIC = Buffer.from(logo.data)
+
+    const media = await message.client.prepareMessage(message.jid, PIC, MessageType.image, { thumbnail: PIC })
+    
+    const buttons = [
+        {buttonId: BUTTHANDLE + 'qaversion', buttonText: {displayText: VER }, type: 1},
+        {buttonId: BUTTHANDLE + 'qasysstats', buttonText: {displayText: SYSDTXT }, type: 1}
+    ]
+    const buttonMessage = {
+        contentText: MSG,
+        footerText: 'Queen Jennifer © ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ',
+        buttons: buttons,
+        headerType: 4,
+        imageMessage: media.message.imageMessage    
+    }
+    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage);
+     
+}));
+
+Julie.addCommand({pattern: 'qasysstats', fromMe: true, desc: Lang.SYSD_DESC, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
+    await QueenAmdi.amdi_setup()
+    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+    await message.sendMessage(
+        '```' + child + '```', MessageType.text, {quoted: message.data}
+    );
+}));
+
+Julie.addCommand({pattern: 'qaversion', fromMe: true, desc: Lang.BOT_V, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
+    await QueenAmdi.amdi_setup()
+    await message.client.sendMessage(message.jid, 
+        `*🧬 Queen Amdi Version 🧬*\n\n` + 
+        '```Installed version :```\n' +
+        Lang.version + 
+        `\n\nCheck official website : https://amdaniwasa.com/`
+   , MessageType.text, {quoted: message.data});
+    
+}));
